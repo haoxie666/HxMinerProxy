c0554cf7463f9dadf900e9bd6935f76301180289
hxminerproxy <96198727+haoxie666@users.noreply.github.com>
2022-04-21 15:04:01 +0800

    脚本更新2.0.1安装

--- linux-install.sh
+++ linux-install.sh
@@ -2,7 +2,7 @@
 # Author: haoxie666
 # github: https://github.com/haoxie666
 
-VERSION="1.1.5"
+VERSION="2.0.1"
 
 DOWNLOAD_HOST="https://github.com/haoxie666/HxMinerProxy/raw/main/Linux-64"
 
@@ -24,6 +24,7 @@ PATH_TURN_ON_SH="/etc/profile.d/HX.sh"
 ISSUE() {
     echo "1.1.2.1"
     echo "1.1.5"
+    echo "2.0.1"
 }
 
 colorEcho(){
@@ -155,7 +156,7 @@ start() {
 }
 
 update() {
-    installapp cu-firs-1.1.5
+    installapp cu-firs-2.0.1
 }
 
 turn_on() {
@@ -395,7 +396,7 @@ read -p "$(echo -e "请选择[1-14]：")" choose
 
 case $choose in
 1)
-    installapp cu-firs-1.1.5
+    installapp cu-firs-2.0.1
     ;;
 2)
     uninstall
