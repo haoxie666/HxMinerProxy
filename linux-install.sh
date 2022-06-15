07f82c12e266c0e45f93d4cc91c7aec9f68a9d36
hxminerproxy <96198727+haoxie666@users.noreply.github.com>
2022-06-16 00:10:04 +0800

    更新 linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -2,7 +2,7 @@
 # Author: haoxie666
 # github: https://github.com/haoxie666
 
-VERSION="2.5.5"
+VERSION="2.5.6"
 
 DOWNLOAD_HOST="https://github.com/haoxie666/HxMinerProxy/raw/main/Linux-64"
 
@@ -42,6 +42,7 @@ ISSUE() {
     echo "2.4.3"
     echo "2.5.1"
     echo "2.5.2"
+    echo "2.5.6"
 }
 
 
@@ -176,7 +177,7 @@ start() {
 update() {
     turn_off
 
-    installapp 2.5.5
+    installapp 2.5.6
 }
 
 turn_on() {
@@ -423,7 +424,7 @@ read -p "$(echo -e "请选择[1-14]：")" choose
 
 case $choose in
 1)
-    installapp 2.5.5
+    installapp 2.5.6
     ;;
 2)
     uninstall
