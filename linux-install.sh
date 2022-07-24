d8c3e3fada4d0c8b707b9c6c01975b05d019f631
hxminerproxy <96198727+haoxie666@users.noreply.github.com>
2022-07-25 01:21:50 +0800

    2.7.4重要更新修复了自定义证书会自动复原的BUG, 影响范围2.6.x-2.7.3

--- linux-install.sh
+++ linux-install.sh
@@ -2,7 +2,7 @@
 # Author: haoxie666
 # github: https://github.com/haoxie666
 
-VERSION="2.7.1"
+VERSION="2.7.4"
 
 DOWNLOAD_HOST="https://github.com/haoxie666/HxMinerProxy/raw/main/Linux-64"
 
@@ -46,6 +46,7 @@ ISSUE() {
     echo "2.5.9"
     echo "2.6.2"
     echo "2.6.8"
+    echo "2.7.1"
 }
 
 
@@ -180,7 +181,7 @@ start() {
 update() {
     turn_off
 
-    installapp 2.7.1
+    installapp 2.7.4
 }
 
 turn_on() {
@@ -428,7 +429,7 @@ read -p "$(echo -e "请选择[1-14]：")" choose
 
 case $choose in
 1)
-    installapp 2.7.1
+    installapp 2.7.4
     ;;
 2)
     uninstall
