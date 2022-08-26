9e61ab66b7d48f27559b7e2932fb536f468dbbdf
hxminerproxy <96198727+haoxie666@users.noreply.github.com>
2022-08-26 16:26:12 +0800

    update version

--- linux-install.sh
+++ linux-install.sh
@@ -2,7 +2,7 @@
 # Author: haoxie666
 # github: https://github.com/haoxie666
 
-VERSION="2.8.0"
+VERSION="2.8.1"
 
 DOWNLOAD_HOST="https://github.com/haoxie666/HxMinerProxy/raw/main/Linux-64"
 
@@ -50,6 +50,7 @@ ISSUE() {
     echo "2.7.4"
     echo "2.7.6"
     echo "2.7.9"
+    echo "2.8.0"
 }
 
 
@@ -184,7 +185,7 @@ start() {
 update() {
     turn_off
 
-    installapp 2.8.0
+    installapp 2.8.1
 }
 
 turn_on() {
@@ -432,7 +433,7 @@ read -p "$(echo -e "请选择[1-14]：")" choose
 
 case $choose in
 1)
-    installapp 2.8.0
+    installapp 2.8.1
     ;;
 2)
     uninstall
